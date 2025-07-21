7abb40f22c8cfd5248ec88a165e5a5ec733b7ffc
AyaazEllemdeen <ayaaz@seo-company.io>
2025-07-21 08:24:33 +0200

    css modal update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -158,8 +158,59 @@
 
                     <div id="step4" class="modal-step1">
                         <h3 class="modal-question1">What state do you live in?</h3>
-                        <input type="text" id="user-state" name="user-state" class="modal-dropdown1"
-                            placeholder="Enter your state" required />
+                        <select id="user-state" name="user-state" class="modal-dropdown1" required>
+                            <option value="">Select your state</option>
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                        </select>
                         <button id="to-step5" class="to-step-btn1">Continue</button>
                     </div>
 
@@ -200,12 +251,13 @@
         </form>
 
         <!-- RIGHT: Logo + Info -->
-        <div id="quiz-modal-right">
+        <div id="quiz-modal-right" class="modal-right">
             <img src="{{ asset('img/logo.png') }}" alt="Company Logo" />
             <h2>Best Extended Auto Warranty in May 2025</h2>
             <p>Continue our short quiz to get matched to an auto warranty provider suited to you.</p>
         </div>
     </div>
+
     @if(session('chaiz_search_data'))
         <div id="response-modal" class="response-modal-overlay">
             <div class="response-modal-wrapper">
