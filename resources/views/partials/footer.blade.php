62408af42f1c4b52dc45cbca5afd77b16d0ff450
AyaazEllemdeen <ayaaz@seo-company.io>
2025-07-22 15:14:07 +0200

    update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,24 +1,16 @@
 <footer class="bg-black text-light pt-5">
     <div class="container">
         <div class="row align-items-center text-center text-lg-start">
-            <!-- Quick Links -->
-            <div class="col-lg-6 mb-4 mb-lg-0">
-                <h5 class="text-warning mb-3">Quick Links</h5>
-                <ul class="list-unstyled">
-                    <li><a href="#home" class="text-light text-decoration-none">Home</a></li>
-                    <li><a href="#faq" class="text-light text-decoration-none">FAQ</a></li>
-                    <li><a href="#blog" class="text-light text-decoration-none">Blog</a></li>
-                </ul>
-            </div>
+            <!-- Removed Quick Links -->
 
             <!-- Logo -->
-            <div class="col-lg-6 text-center text-lg-end">
+            <div class="col-lg-6 offset-lg-6 text-center text-lg-end">
                 <img src="{{ asset('img/logo.png') }}" alt="Compare Warranties Logo" style="height: 60px;">
             </div>
         </div>
 
-
         <hr class="border-secondary my-4">
+
         <!-- Bottom Bar -->
         <div class="row footer-bottom">
             <div class="col text-center">
