b48dc79440ab48ce7c9cb90eeecd44433a58c5b1
AyaazEllemdeen <ayaaz@seo-company.io>
2025-07-24 09:31:56 +0200

    check

---
+++ resources/views/home2.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <script>
+      window.chaizWarrantySearchConfig = {
+        targetElementId: 'search-results',
+        searchData: {
+          make: 'mercedes-benz',
+          model: 'gle coupe',
+          year: 2020,
+          state: 'NJ',
+          mileage: 30000,
+          userId: '96d8841b-6ae6-4cb6-9b43-401662e25560'
+        }
+      };
+    </script>
+    <script src="https://uat.warranty-search.chaiz.com/initialize.js"></script>
+  </head>
+  <body>
+    <div id='search-results'></div>
+  </body>
+</html>
