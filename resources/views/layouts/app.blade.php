a55d32661d0b18d14e701bd3d80a1d31ca286aa2
AyaazEllemdeen <ayaaz@seo-company.io>
2025-07-24 16:04:47 +0200

    change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'MySite')</title>
+    <title>@yield('title', 'Cworg')</title>
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
 
