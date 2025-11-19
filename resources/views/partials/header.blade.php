3297d1920b3e81c47346e3eed17807fc8f5f7280
AyaazEllemdeen <ayaaz@seo-company.io>
2025-11-19 15:38:05 +0200

    new design

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,10 +8,10 @@
         </div>
 
         <!-- Hamburger icon on the right using SVG -->
-        {{-- <button class="btn p-0" type="button" aria-label="Toggle menu" style="background: transparent; border: none;">
+        <button class="btn p-0" type="button" aria-label="Toggle menu" style="background: transparent; border: none;">
             <svg xmlns="http://www.w3.org/2000/svg" width="51" height="51" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
             </svg>
-        </button> --}}
+        </button>
     </div>
 </header>
