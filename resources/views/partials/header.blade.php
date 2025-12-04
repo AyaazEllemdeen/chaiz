a304ded9b4b1413e69aa22f44ad5243a32d30972
AyaazEllemdeen <ayaaz@seo-company.io>
2025-12-04 10:25:44 +0200

    iphone

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,19 @@
 <header class="site-header bg-white shadow-sm">
     <div class="container d-flex justify-content-between align-items-center py-3">
         <!-- Logo on the left -->
-        <div class="logo-wrap">
+        <div style="flex-shrink: 0;">
             <a href="{{ url('/') }}">
                 <img src="{{ asset('img/logo/logo.png') }}" alt="Logo" style="height: 45px;">
             </a>
         </div>
 
         <!-- Hamburger icon on the right using SVG -->
-        {{-- <button class="btn p-0" type="button" aria-label="Toggle menu" style="background: transparent; border: none;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="51" height="51" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+        {{-- <button class="btn p-0" type="button" aria-label="Toggle menu"
+            style="background: transparent; border: none;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="51" height="51" fill="currentColor" class="bi bi-list"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
             </svg>
         </button> --}}
     </div>
